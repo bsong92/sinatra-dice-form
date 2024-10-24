390022b514c2dc318c8634f46a432150a5653894
Brian Song <brian.cs.song@gmail.com>
2024-10-24 17:07:45 +0000

    Update the params

--- app.rb
+++ app.rb
@@ -13,7 +13,11 @@ get ("/process_roll") do
 
   @num_sides = params.fetch("sides").to_i
 
-  #
-  
+  @rolls = []
+
+  @num_dice.times do
+    @rolls.push(rand(1..@num_sides))
+  end
+
   erb(:results)
 end
